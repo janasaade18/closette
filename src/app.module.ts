b1ab9d18aecd213c3f7cbd2788d4511b99d1a987
Jana Saade <202213017@ua.edu.lb>
2026-07-11 19:08:12 +0300

    feat: connect MongoDB Atlas via Mongoose

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
@@ -9,9 +10,15 @@ import { AppService } from './app.service';
       isGlobal: true,
       envFilePath: '.env',
     }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGODB_URI'),
+      }),
+    }),
   ],
   controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
-//test
